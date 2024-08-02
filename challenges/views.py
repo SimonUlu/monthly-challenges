b7cd3474b945bd05f9cbc57e15377a1a7e261e0e
sunterlugauer <s.unterlugauer@keyed.de>
2024-08-02 16:05:38 +0200

    fixed bug by putting int before str

--- challenges/views.py
+++ challenges/views.py
@@ -4,15 +4,8 @@ from django.http import HttpResponse, HttpResponseNotFound
 # Create your views here.
 
 def monthly_challenge_by_number(request, month):
-    challenge_text = None
-    if month == 1:
-       challenge_text = "January"
-    elif month == 2:
-        challenge_text = "February"
-    else:
-        return HttpResponseNotFound("Month is not supported")
+    return HttpResponse(month)
         
-    return HttpResponse(challenge_text)
 
 def monthly_challenge(request, month):
     challenge_text = None
