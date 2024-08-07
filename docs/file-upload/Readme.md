5a35957254006205fefdec9656e5ff31ba62349a
sunterlugauer <s.unterlugauer@keyed.de>
2024-08-07 16:16:03 +0200

    added new logic to upload images and temp folder

---
+++ docs/file-upload/Readme.md
@@ -0,0 +1,25 @@
+## Create post form with file upload and return
+
+html:
+
+```sh
+
+  <form action="/profiles/" method="POST" enctype="multipart/form-data">
+    {% csrf_token %}
+    <input type="file" name="image" id="image" />
+    <button>Upload!</button>
+  </form>
+
+```
+
+```sh
+class CreateProfileView(View):
+    def get(self, request):
+        return render(request, "profiles/create_profile.html")
+
+    def post(self, request):
+        files = request.FILES["image"]
+        print(files)
+        
+        return redirect("/profiles")
+```
--- profiles/views.py
+++ profiles/views.py
@@ -1,12 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
 
 # Create your views here.
 
+
+def __store_file(file):
+    with open("temp/image.jpg", "wb+") as dest:
+        for chunk in file.chunks():
+            dest.write(chunk)
 
 class CreateProfileView(View):
     def get(self, request):
         return render(request, "profiles/create_profile.html")
 
     def post(self, request):
-        pass
+        files = request.FILES["image"]
+        
+        __store_file(files)
+        
+        return redirect("/profiles")
+
+
+
+        
