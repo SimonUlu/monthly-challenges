67ffa9e052bb9d4e6be2632c76392e791a7d5b08
sunterlugauer <s.unterlugauer@keyed.de>
2024-08-07 12:51:00 +0200

    created new subapp reviews with form and views

--- reviews/views.py
+++ reviews/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
 
 # Create your views here.
+
+
+def index(request):
+    if request.method == "POST":
+        entered_username = request.POST["username"]
+
+        if entered_username == "":
+            return render(request, "reviews/review.html", {"has_error": True})
+
+        return redirect("thank_you")
+    return render(request, "reviews/review.html", {"has_error": True})
+
+
+def thank_you(request):
+    return render(request, "reviews/thank_you.html")
